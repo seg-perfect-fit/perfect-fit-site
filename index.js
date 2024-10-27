3f348f1e6a069d08456d68065605721fe53b57bd
Taha Rashid <taharashid710192@gmail.com>
2024-10-26 23:47:34 -0400

    Major overhaul: added transitions for major elements, applied colour schemes, made webpage dynamic for multiple different screen sizes

--- index.js
+++ index.js
@@ -3,23 +3,30 @@
 // JS file for index.html
 
 // get items to fade in when the screen loads initially
-const appear = (item) => {
+const appear = (item, addedClass="visible") => {
     const selected = document.querySelector(item)
-    selected.classList.add("visible")
+    selected.classList.add(addedClass)
 }
 
 // function that does the delayed fade in
-const appearDelay = async (waitTime, item) => {
+const appearDelay = async (waitTime, item, addedClass) => {
     return new Promise((resolve, reject) => {
         setTimeout(() => { 
-            window.addEventListener("load", appear(item));
+            window.addEventListener("load", appear(item, addedClass));
             resolve();
         }, waitTime);
     })      
 }
 
 // running the fade-in code
-let time = 400
+let time = 300
 appearDelay(time, "#nav-top")
 .then(() => appearDelay(time, ".inside-left"))
 .then (() => appearDelay(time, ".inside-right"))
+.then(() => appearDelay(500, ".inside-right img"))  //avatar picture
+.then (() => appearDelay(500, "#slogan-inner", "slogan-you-visible"))   //changing the colour of "you"
+.then(() => appearDelay(time, "#text-interactive")) //member sign-in
+.then(() => appearDelay(1000, "#main-text"))    //the main text
+
+
+
