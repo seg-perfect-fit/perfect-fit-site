24de7a1604bf61a9481dff601364f313832863d6
Taha Rashid <taharashid710192@gmail.com>
2024-10-31 23:59:54 -0400

    working on the details section of the home page. Added a shirt as a transition background image between main section and details section. made it work on different screen sizes

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 // JS file for index.html
 
 // get items to fade in when the screen loads initially
-const appear = (item, addedClass="visible") => {
+const appear = (item, addedClass = "visible") => {
     const selected = document.querySelector(item)
     selected.classList.add(addedClass)
 }
@@ -11,22 +11,39 @@ const appear = (item, addedClass="visible") => {
 // function that does the delayed fade in
 const appearDelay = async (waitTime, item, addedClass) => {
     return new Promise((resolve, reject) => {
-        setTimeout(() => { 
+        setTimeout(() => {
             window.addEventListener("load", appear(item, addedClass));
             resolve();
         }, waitTime);
-    })      
+    })
 }
 
 // running the fade-in code
 let time = 300
 appearDelay(time, "#nav-top")
-.then(() => appearDelay(time, ".inside-left"))
-.then (() => appearDelay(time, ".inside-right"))
-.then(() => appearDelay(500, ".inside-right img"))  //avatar picture
-.then (() => appearDelay(500, "#slogan-inner", "slogan-you-visible"))   //changing the colour of "you"
-.then(() => appearDelay(time, "#text-interactive")) //member sign-in
-.then(() => appearDelay(1000, "#main-text"))    //the main text
+    .then(() => appearDelay(time, ".inside-left"))
+    .then(() => appearDelay(time, ".inside-right"))
+    .then(() => appearDelay(500, ".inside-right img"))  //avatar picture, 500delay
+    .then(() => appearDelay(500, "#slogan-inner", "slogan-you-visible"))   //changing the colour of "you", 500delay
+    .then(() => appearDelay(time, "#text-interactive")) //member sign-in
+    .then(() => appearDelay(1000, "#main-text"))    //the main text, 1000delay
+
+//properly scale the details div with the shirt image
+const dynamicSize = () => {
+    const shirt = document.querySelector("#background-img-details img");
+    const details = document.querySelector("#main-text");
+    //if we want to change the position of the div relative to the shirt, we can /100 * a % we want it to be down
+    //to be fully below, just *100!
+    let shirtHeight = (shirt.height / 100) * 68;
+
+    details.style.marginTop = `${shirtHeight}px`;
+}
+
+// apply the scaling changes as the screen size changes, and run it initially
+dynamicSize();
+window.onresize = function() {
+    dynamicSize();
+}
 
 
 
